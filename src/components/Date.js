cb4b53a3b723b577c7841006d03a3829ce3cf5dc
Sun971 <aisorya.bhandari@gmail.com>
2022-01-18 23:49:19 -0500

    Add animate feature on picture deck

--- src/components/Date.js
+++ src/components/Date.js
@@ -4,7 +4,7 @@ import './Form.css';
 const Date = props => 
 <form onSubmit = {props.changeDate} style={{ display: "center"}}>
     <input type="date"className='dateForm' />
-    <input type="image" id="rocket" alt="Find" src={require(".//images/rocket.png")} className='submitButton'/>
+    <input type="image" id="rocket" alt="Find" src={require("./images/rocket.png")} className='submitButton'/>
 </form>
 
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './Card.css';
 
-const Photo = props =>
-    <div>
-        <h3>{props.photo.title}</h3>
-        <img src={props.photo.url} alt={props.photo.title} />
-        <p>{props.photo.date}</p>
-        <p>{props.photo.explanation}</p>    
-        </div>;
+const Main = props =>
+    <div className='card-deck'>
+        <div className='title-img'>
+            <h3>{props.photo.title}</h3>  
+        </div>
+        <div className='img-of-day'> 
+            <img src={props.photo.url} alt={props.photo.title} />
+        </div> 
+        <div className='date'> 
+            <p>{props.photo.date}</p>
+        </div>
+        <div className='explanation'> 
+            <p>{props.photo.explanation}</p>
+        </div>    
+    </div>;
 
-    export default Photo;
+export default Main;
