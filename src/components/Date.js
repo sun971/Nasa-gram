900195bc7ae33e4df2323ba10b8bea3a701a4c44
Sun971 <aisorya.bhandari@gmail.com>
2022-01-18 22:07:05 -0500

    Fix date selector and search button

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import './Form.css';
 
 const Date = props => 
-<form onSubmit = {props.changeDate}>
-    Enter a Date (YYYY-MM-DD):
-    <input />
-    <input type="submit" />
+<form onSubmit = {props.changeDate} style={{ display: "center"}}>
+    <input type="date"className='dateForm' />
+    <input type="image" id="rocket" alt="Find" src={require(".//images/rocket.png")} className='submitButton'/>
 </form>
 
 
-export default Date;
+export default Date; 
